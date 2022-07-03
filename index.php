a94bbe861928715f0ec54bb3aa91d1ddd3bb7871
Diogo Jesus <diogo.joao@tutanota.com>
2022-07-03 23:50:35 +0100

    1- Inicio Login

---
+++ index.php
@@ -0,0 +1,24 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+ </head>
+ <body>
+    <nav>
+        <div class="wrapper">
+            <a href="index.php"><img src="img/logo-white.png" alt="Blogs logo" srcset=""></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="discover.php">Login</a></li>
+                <li><a href="blog.php">Register</a></li>
+                <li><a href="signup.php">Register</a></li>
+                <li><a href="login.php">Register</a></li>
+            </ul>
+        </div>
+    </nav>
+ </body>
+ </html>
