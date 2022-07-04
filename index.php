9ecf7901c45be3ab3115074a8f0e205b600efc33
Diogo Jesus <diogo.joao@tutanota.com>
2022-07-04 18:56:22 +0100

    2-singup page done second adition

--- index.php
+++ index.php
@@ -1,24 +1,31 @@
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
- </head>
- <body>
-    <nav>
-        <div class="wrapper">
-            <a href="index.php"><img src="img/logo-white.png" alt="Blogs logo" srcset=""></a>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="discover.php">Login</a></li>
-                <li><a href="blog.php">Register</a></li>
-                <li><a href="signup.php">Register</a></li>
-                <li><a href="login.php">Register</a></li>
-            </ul>
+<?php
+include_once "header.php";
+?>
+
+     <div class="wrapper">
+         <section class="index-intro">
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, impedit! A tempore est perspiciatis itaque at. Natus, cum beatae necessitatibus soluta rerum quaerat quibusdam expedita dolores, consectetur blanditiis qui. Recusandae?</p>
+         </section>
+        <section class="index-categories">
+            <h2>basic categories</h2>
+            <div class="index-categories-list">
+                <ul>
+                <div>    
+                <li><a href="">Category 1</a></li>
+                </div>
+                <div>
+                <li><a href="">Category 2</a></li>
+                </div>
+                <div>
+                <li><a href="">Category 3</a></li>
+                </div>
+                </ul>
+            
+            </div>
+        </section>
+
         </div>
-    </nav>
- </body>
- </html>
+
+<?php
+include_once "footer.php";
+?>
