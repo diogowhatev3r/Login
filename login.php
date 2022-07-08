374d995e60a06f944a8fbdd2994774cf4a7f7516
Diogo Jesus <diogo.joao@tutanota.com>
2022-07-08 17:07:35 +0100

    4 - Ficheiros de base de dados adicionados.

--- login.php
+++ login.php
@@ -1,6 +1,25 @@
 <?php
 include_once "header.php";
 
-include_once "footer.php";
 ?>
 
+
+<section class="signup-form">
+  <h2>Sign Up</h2>
+  <div class="signup-form-form">
+    <form action="includes/signup.inc.php" method="post">
+     
+      <input type="text" name="uid" placeholder="Login name">
+     
+      <input type="password" name="pwd" placeholder="Password...">
+     
+      <button type="submit" name="submit">Login</button>
+    </form>
+  </div>
+</section>
+
+
+
+<?php
+include_once "footer.php";
+?>
