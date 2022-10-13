7513ffeec88177b20802fa978830225b6cafc828
Diogo Jesus <diogo.joao@tutanota.com>
2022-10-13 16:07:59 +0100

    01-Primeiro commit da branch

--- index.php
+++ index.php
@@ -2,29 +2,29 @@
 include_once "header.php";
 ?>
 
-     <div class="wrapper">
-         <section class="index-intro">
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, impedit! A tempore est perspiciatis itaque at. Natus, cum beatae necessitatibus soluta rerum quaerat quibusdam expedita dolores, consectetur blanditiis qui. Recusandae?</p>
-         </section>
-        <section class="index-categories">
-            <h2>basic categories</h2>
-            <div class="index-categories-list">
-                <ul>
-                <div>    
-                <li><a href="">Category 1</a></li>
+<div class="wrapper">
+    <section class="index-intro">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, impedit! A tempore est perspiciatis itaque at. Natus, cum beatae necessitatibus soluta rerum quaerat quibusdam expedita dolores, consectetur blanditiis qui. Recusandae?</p>
+    </section>
+    <section class="index-categories">
+        <h2>basic categories</h2>
+        <div class="index-categories-list">
+            <ul>
+                <div>
+                    <li><a href="">Category 1</a></li>
                 </div>
                 <div>
-                <li><a href="">Category 2</a></li>
+                    <li><a href="">Category 2</a></li>
                 </div>
                 <div>
-                <li><a href="">Category 3</a></li>
+                    <li><a href="">Category 3</a></li>
                 </div>
-                </ul>
-            
-            </div>
-        </section>
+            </ul>
 
         </div>
+    </section>
+
+</div>
 
 <?php
 include_once "footer.php";
